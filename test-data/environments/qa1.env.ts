57bf682876c07eaa90ec980567054ae9fcf25890
Kubilay Dogan <kubilaydogan.us@gmail.com>
2025-10-04 07:47:52 -0400

    github secret implementation

--- test-data/environments/qa1.env.ts
+++ test-data/environments/qa1.env.ts
@@ -1,6 +1,6 @@
 export default {
     username: 'standard_user',
-    password: 'secret_sauce',
+    password: process.env.SAUCE_PASSWORD,
     products: {
         item1: 'Sauce Labs Backpack',
         item2: 'Sauce Labs Bolt T-Shirt',
